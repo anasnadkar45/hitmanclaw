838c6e9137e2a9dede98135f4fe581ddd43ef7a9
anasnadkar45 <anasnadkar23@gmail.com>
2026-05-31 21:02:52 +0530

    feat: add wakeup UI with banner and mode selection

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 #!/usr/bin/env bun
 
 import { Command } from "commander";
+import { runWakeup } from "./tui/wakeup";
 
 const program = new Command();
 
@@ -13,7 +14,7 @@ program
     .command("wakeup")
     .description("show the banner and pick cli or telegram mode")
     .action(async()=>{
-        console.log("Wakeup calling....")
+        await runWakeup();
     })
 
 await program.parseAsync(process.args)
